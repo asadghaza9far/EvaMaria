0390db52a4e84cc07b4bfd836680137bad757a70
asadghaza9far <145460765+asadghaza9far@users.noreply.github.com>
2023-09-24 12:02:29 +0500

    start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/asadghaza9far/EvaMaria/tree/master
+  git clone https://github.com/asadghaza9far/EvaMaria
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /EvaMaria
